6c66dd729508bb4e15f4d0fc7849517a03b05b60
ufo305 <sycsnote5@gmail.com>
2022-05-03 23:41:54 +0900

    update 0503
    2nd

--- sns_project/settings.py
+++ sns_project/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'sns_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'basic', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,3 +126,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#STATIC 상속 설정
+STATICFILEDS_DIRS = [
+    os.path.join(BASE_DIR, ' basic', 'static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
